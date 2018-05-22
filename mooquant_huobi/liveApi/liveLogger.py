d5fb3c5a5b0909a083f9a76d3c31cdb4e7b24748
Travis CI <travis@travis-ci.org>
2018-05-22 15:25:54 +0800

    update commit

---
+++ mooquant_huobi/liveApi/liveLogger.py
@@ -0,0 +1,31 @@
+import logging
+
+from mooquant import logger
+
+from . import liveUtils
+
+log_format = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+level = logging.INFO
+file_log = "/tmp/mooquant_huobi.log"
+
+fileHandler = None
+
+
+class Formatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        return "[%s]" % liveUtils.localTime()
+
+
+def initLogger(logger):
+    global fileHandler
+    if fileHandler is None:
+        fileHandler = logging.FileHandler(file_log)
+        fileHandler.setFormatter(log_format)
+        fileHandler.setFormatter(Formatter(log_format))
+    logger.addHandler(fileHandler)
+
+
+def getLiveLogger(name):
+    log = logger.getLogger(name)
+    initLogger(log)
+    return log
